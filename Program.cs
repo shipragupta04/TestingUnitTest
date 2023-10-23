5799c84e24c4737e90166513bd26d10706789312
Gupta <shipragupta@rocketloans.com>
2023-10-23 16:17:21 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static TestingUnitTest.BasicMaths;
+
+
+namespace TestingUnitTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            Console.Write("enter first number");
+            var x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter Second number");
+            var y = Convert.ToDouble(Console.ReadLine());
+            var bm = new BasicMaths();
+            Console.WriteLine(bm.Add(x, y));
+
+
+        }
+    }
+}
